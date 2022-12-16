20d42e0f88bb96693f6458f1ad83446024458249
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-16 10:54:32 -0300

    Ajusta layout default

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
-export const LayoutContainer = styled.div``
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+
+  background: ${(props) => props.theme.background};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`

5d622f24ae1cb52c7378a1c7df10cdf647441a6d
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-16 10:51:49 -0300

    Adiciona cores defaults para a aplicação

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,4 +1,4 @@
-export const defaultTheme = {
+export const defaultColors = {
   white: '#FFF',
 
   'gray-100': '#FAFAFA',
@@ -15,6 +15,7 @@ export const defaultTheme = {
   'green-500': '#00875F',
   'green-700': '#015F43',
 
+  'red-100': '#EB5757',
   'red-500': '#AB222E',
   'red-700': '#7A1921',
 
@@ -26,4 +27,42 @@ export const defaultTheme = {
   'purple-100': '#EBE5F9',
   'purple-200': '#8047F8',
   'purple-300': '#4B2995',
+}
+
+export const defaultTheme = {
+  white: defaultColors.white,
+  background: defaultColors['gray-100'],
+  card: defaultColors['gray-200'],
+  input: defaultColors['gray-300'],
+  button: defaultColors['gray-400'],
+  hover: defaultColors['gray-500'],
+  label: defaultColors['gray-600'],
+  text: defaultColors['gray-700'],
+  subtitle: defaultColors['gray-800'],
+  title: defaultColors['gray-900'],
+  primary: {
+    dark: defaultColors['purple-300'],
+    normal: defaultColors['purple-200'],
+    light: defaultColors['purple-100'],
+  },
+  secondary: {
+    dark: defaultColors['yellow-600'],
+    normal: defaultColors['yellow-400'],
+    light: defaultColors['yellow-100'],
+  },
+  danger: {
+    dark: defaultColors['red-700'],
+    normal: defaultColors['red-500'],
+    light: defaultColors['red-100'],
+  },
+  warning: {
+    dark: defaultColors['yellow-600'],
+    normal: defaultColors['yellow-500'],
+    light: defaultColors['yellow-100'],
+  },
+  success: {
+    dark: defaultColors['green-700'],
+    normal: defaultColors['green-500'],
+    light: defaultColors['green-300'],
+  },
 }

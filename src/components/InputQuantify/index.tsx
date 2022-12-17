49b0d609c207ccff27c7d932b5c92bda5f1f1959
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-17 13:36:14 -0300

    Adiciona função de aumentar e diminuir o valor do input de quantidade

--- src/components/InputQuantify/index.tsx
+++ src/components/InputQuantify/index.tsx
@@ -1,15 +1,39 @@
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import { InputQuantifyContainer } from './styles'
 
 export function InputQuantify() {
+  const [quantify, setQuantity] = useState<number>(1)
+
+  function handleQuantify(type: 'sub' | 'add'): void {
+    let qtd = quantify
+
+    switch (type) {
+      case 'sub':
+        if (qtd > 1) qtd -= 1
+        break
+      case 'add':
+        qtd += 1
+        break
+    }
+
+    setQuantity(qtd)
+  }
+
   return (
     <InputQuantifyContainer>
-      <button>
+      <button onClick={() => handleQuantify('sub')}>
         <FontAwesomeIcon icon={faMinus} />
       </button>
-      <input type="text" min="1" defaultValue={1} />
-      <button>
+      <input
+        type="text"
+        min="1"
+        defaultValue={1}
+        value={quantify}
+        onBlur={(e) => setQuantity(parseInt(e.target.value))}
+      />
+      <button onClick={() => handleQuantify('add')}>
         <FontAwesomeIcon icon={faPlus} />
       </button>
     </InputQuantifyContainer>
